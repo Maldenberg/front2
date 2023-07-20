9458416fc35bee6d0ff1e16e7650c3cb6798351b
Maldenberg <96823272+Maldenberg@users.noreply.github.com>
2023-07-20 23:57:16 +0300

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = '<h3> Gule Gule </h3> <button class="button2">Пошел нахуй</button>'
+});
+
